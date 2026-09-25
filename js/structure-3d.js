102c3a86ff63586617e6ee43ed09163a760afb0d
satrioismu2405216-a11y <satrio.ismu.2405216@students.um.ac.id>
2026-09-25 22:00:14 +0700

    Update structure-3d.js

--- js/structure-3d.js
+++ js/structure-3d.js
@@ -1,1 +1,31 @@
-
+  setSpanLength(lengthMeter) {
+    this.beamLength = Math.max(4, Math.min(14, lengthMeter));
+    this.loads.forEach(l => {
+      if (l.pos > this.beamLength - 0.5) l.pos = this.beamLength / 2;
+    });
+    this.buildStructures();
+  }
+  resetView() {
+    if (this.controls) {
+      this.controls.reset();
+      this.camera.position.set(0, 4.8, 11.5);
+      this.controls.target.set(0, 1.4, 0);
+    }
+  }
+  onWindowResize() {
+    if (!this.container || !this.renderer || !this.camera) return;
+    const width = this.container.clientWidth;
+    const height = this.container.clientHeight;
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(width, height);
+  }
+  animate() {
+    requestAnimationFrame(() => this.animate());
+    if (this.controls) this.controls.update();
+    if (this.renderer && this.scene && this.camera) {
+      this.renderer.render(this.scene, this.camera);
+    }
+  }
+}
+window.Structure3DViewer = Structure3DViewer;
